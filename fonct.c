0b101f7a68159d7df2d0dfd91fd45e45835e5270
RulerSteps <Diagnemalick163@gmail.com>
2025-04-09 01:57:32 +0000

    Create fonct.c

---
+++ fonct.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+float Addition(float a, float b)
+{
+	float Resultat;
+	Resultat = a + b;
+	return Resultat;
+}
+
+float Soustraction(float a, float b)
+{
+	float Resultat;
+	Resultat = a -b;
+	return Resultat;
+}
+
+float Multiplication(float a, float b)
+{
+	float Resultat;
+	Resultat = a*b;
+	return Resultat;
+}
+
+float Division(float a, float b)
+{
+	float Resultat;
+	Resultat = a/b;
+	return Resultat;
+	
+}
